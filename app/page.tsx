2231398b04ccf84a9b3ffdb4473fe3e4666b19a2
KennethMinn <minthukha2472005@gmail.com>
2023-11-09 19:17:01 +0630

    added the billboard in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/contianer";
 
 const Home = () => {
-  return <div className="">Home</div>;
+  return (
+    <Container>
+      <div className=" space-y-10 pb-10">
+        <Billboard
+          label="Explore the featured Products"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR78GpbfqMSXlW6QaFXVZdxF6jLNx8Au_qfRVEJ4aAICzDgB13BFmF-pO-LAqarfuKDr44&usqp=CAU"
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default Home;

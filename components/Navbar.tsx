4bb58936be5b7ff432aead9ae9131996e4b1ca74
KennethMinn <minthukha2472005@gmail.com>
2023-11-10 11:58:47 +0630

    added mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,17 @@
 
 import { categories, cn } from "@/lib/utils";
 import { UserButton, useAuth } from "@clerk/nextjs";
+import { AlignJustify, X } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import { Separator } from "./ui/separator";
 
 const Navbar = () => {
   const pathname = usePathname();
   const { userId } = useAuth();
+  const [open, setOpen] = useState(false);
 
-  console.log(pathname);
   const routes = categories.map((category) => ({
     label: category.replace(category[0], category[0].toUpperCase()),
     href: `/${category}`,
@@ -22,7 +24,7 @@ const Navbar = () => {
       <Link href={"/"}>
         <h1 className="font-bold text-3xl me-8 border-r-2 pr-8">LOgo</h1>
       </Link>
-      <div className="flex justify-center items-center gap-x-8">
+      <div className="md:flex justify-center items-center gap-x-8 hidden">
         {routes.map((route) => (
           <Link
             href={route.href}
@@ -36,7 +38,7 @@ const Navbar = () => {
           </Link>
         ))}
       </div>
-      <div className="ml-auto flex gap-3 items-center">
+      <div className="ml-auto  md:flex gap-3 items-center hidden">
         {!userId && (
           <Link
             className=" bg-black text-white rounded-md py-1 px-2"
@@ -55,6 +57,60 @@ const Navbar = () => {
           {userId === process.env.NEXT_PUBLIC_ADMIN_ID && "Dashboard"}
         </Link>
         <UserButton afterSignOutUrl="/" />
+      </div>
+
+      {/* mobile Navbar */}
+      <div className=" ml-auto md:hidden">
+        <div className="relative select-none flex items-center gap-3">
+          <div className=" " onClick={() => setOpen((prev) => !prev)}>
+            {open ? <X /> : <AlignJustify size={25} />}
+          </div>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+        {open && (
+          <div className=" bg-gray-300 px-8 py-2 absolute z-10 right-[55px] rounded-md">
+            <div className=" flex flex-col gap-2">
+              {routes.map((route) => (
+                <Link
+                  href={route.href}
+                  key={route.href}
+                  className={cn(
+                    " text-gray-400",
+                    route.active ? " text-black" : "text-gray-400"
+                  )}
+                  onClick={() => setOpen(false)}
+                >
+                  {route.label}
+                </Link>
+              ))}
+              <div className="">
+                {!userId && (
+                  <Link
+                    href={"/sign-in"}
+                    className={cn(" text-gray-400")}
+                    onClick={() => setOpen(false)}
+                  >
+                    Sign in
+                  </Link>
+                )}
+                {userId === process.env.NEXT_PUBLIC_ADMIN_ID && (
+                  <Link
+                    href="/dashboard"
+                    className={cn(
+                      " text-gray-400",
+                      pathname === "/dashboard"
+                        ? " text-black"
+                        : "text-gray-400"
+                    )}
+                    onClick={() => setOpen(false)}
+                  >
+                    Dashboard
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
